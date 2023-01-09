8f1e1ea1c7d58323996d35b32f85597b818aac79
Ondřej Čertík <ondrej@certik.us>
2023-01-09 14:01:35 -0700

    Fix the headers and return 0.

--- src/fa.cpp
+++ src/fa.cpp
@@ -1,8 +1,8 @@
-#include "clang/AST/ASTConsumer.h"
-#include "clang/AST/RecursiveASTVisitor.h"
-#include "clang/Frontend/CompilerInstance.h"
-#include "clang/Frontend/FrontendAction.h"
-#include "clang/Tooling/Tooling.h"
+#include <clang/AST/ASTConsumer.h>
+#include <clang/AST/RecursiveASTVisitor.h>
+#include <clang/Frontend/CompilerInstance.h>
+#include <clang/Frontend/FrontendAction.h>
+#include <clang/Tooling/Tooling.h>
 
 using namespace clang;
 
@@ -51,4 +51,5 @@ int main(int argc, char **argv) {
   if (argc > 1) {
     clang::tooling::runToolOnCode(std::make_unique<FindNamedClassAction>(), argv[1]);
   }
+  return 0;
 }
