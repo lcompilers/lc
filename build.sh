1ec5b2081ee60298e402e35d5b70f16a0c3b606c
Ondřej Čertík <ondrej@certik.us>
2023-01-30 14:01:16 -0700

    Build libasr

--- build.sh
+++ build.sh
@@ -2,6 +2,12 @@
 
 set -ex
 
+cd libasr
+python src/libasr/asdl_cpp.py src/libasr/ASR.asdl src/libasr/asr.h
+python src/libasr/wasm_instructions_visitor.py
+cd ..
+
+
 cmake \
     -DCMAKE_BUILD_TYPE=Debug \
     -DWITH_LLVM=yes \
