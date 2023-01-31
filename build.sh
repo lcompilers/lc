e9b455b35074375f3f77295b3fe059fcf2792126
Ondřej Čertík <ondrej@certik.us>
2023-01-31 13:59:45 -0700

    Build in a build directory

--- build.sh
+++ build.sh
@@ -7,9 +7,11 @@ python src/libasr/asdl_cpp.py src/libasr/ASR.asdl src/libasr/asr.h
 python src/libasr/wasm_instructions_visitor.py
 cd ..
 
+mkdir build
+cd build
 
 cmake \
     -DCMAKE_BUILD_TYPE=Debug \
     -DWITH_LLVM=yes \
-    .
+    ..
 cmake --build . -j16
