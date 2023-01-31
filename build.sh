cf8c75a653a4f528b5bb65da27e877ee251b6599
Ondřej Čertík <ondrej@certik.us>
2023-01-31 14:06:32 -0700

    Generate header files into build/

--- build.sh
+++ build.sh
@@ -5,8 +5,16 @@ set -ex
 python libasr/src/libasr/asdl_cpp.py libasr/src/libasr/ASR.asdl libasr/src/libasr/asr.h
 python libasr/src/libasr/wasm_instructions_visitor.py
 
+rm -rf build
 mkdir build
+
+mkdir -p build/libasr/src/libasr
+mv libasr/src/libasr/asr.h build/libasr/src/libasr
+mv libasr/src/libasr/wasm_visitor.h build/libasr/src/libasr
+
 cd build
+
+
 
 cmake \
     -DCMAKE_BUILD_TYPE=Debug \
