776697fd125be81e52b8e4741ef96ba9759dfbcf
Ondřej Čertík <ondrej@certik.us>
2023-01-31 14:07:36 -0700

    Cleanup libasr

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -ex
 
 python libasr/src/libasr/asdl_cpp.py libasr/src/libasr/ASR.asdl libasr/src/libasr/asr.h
 python libasr/src/libasr/wasm_instructions_visitor.py
+rm -rf libasr/src/libasr/__pycache__/
 
 rm -rf build
 mkdir build
