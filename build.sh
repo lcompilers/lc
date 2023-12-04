6ebc5f85a8adaee6d4c27e60d5ad06ef815a9033
Shaikh Ubaid <shaikhubaid769@gmail.com>
2023-12-04 12:58:12 +0530

    Update the build instructions

--- build.sh
+++ build.sh
@@ -4,21 +4,9 @@ set -ex
 
 python src/libasr/asdl_cpp.py src/libasr/ASR.asdl src/libasr/asr.h
 python src/libasr/wasm_instructions_visitor.py
-rm -rf src/libasr/__pycache__/
-
-rm -rf build
-mkdir build
-
-mkdir -p build/src/libasr
-mv src/libasr/asr.h build/src/libasr
-mv src/libasr/wasm_visitor.h build/src/libasr
-
-cd build
-
-
 
 cmake \
     -DCMAKE_BUILD_TYPE=Debug \
     -DWITH_LLVM=yes \
-    ..
+    .
 cmake --build . -j16
