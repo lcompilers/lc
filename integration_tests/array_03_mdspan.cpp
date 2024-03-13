449efc012cce6f7cbfc5eecc5f60408c656e7e71
Gagandeep Singh <gdp.1807@gmail.com>
2024-03-14 00:47:46 +0530

    DEV: Port array_03.cpp to Kokkos::mdspan

---
+++ integration_tests/array_03_mdspan.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <mdspan/mdspan.hpp>
+
+void print(Kokkos::mdspan<double, Kokkos::dextents<int32_t, 1>>& arr) {
+    for( int32_t i = 0; i < arr.size(); i++ ) {
+        std::cout << arr[i] << std::endl;
+    }
+}
+
+int main() {
+
+    std::vector<double> arr1_data = {1.0, 2.0, 3.0, 2.0, 5.0, 7.0, 2.0, 5.0, 7.0};
+    std::vector<double> arr2_data = {5.0, 6.0, 7.0};
+
+    Kokkos::mdspan<double, Kokkos::dextents<int32_t, 2>> arr1{arr1_data.data(), 3, 3};
+
+    Kokkos::mdspan<double, Kokkos::dextents<int32_t, 1>> arr2{arr2_data.data(), 3};
+
+    print( arr2 );
+
+    return 0;
+}
