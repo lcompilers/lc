454d12fc1c198bd619c82c2140461901625c37b7
Gagandeep Singh <gdp.1807@gmail.com>
2024-03-21 21:35:39 +0530

    TEST: Added verification checks for values in res

--- integration_tests/array_04.cpp
+++ integration_tests/array_04.cpp
@@ -13,11 +13,17 @@ int main() {
 
     xt::xtensor<double, 1> arr2 {5.0, 6.0, 7.0};
     xt::xtensor<double, 1> res;
+    xt::xtensor_fixed<double, xt::xshape<3>> res_ = {7.0, 11.0, 14.0};
 
     res = xt::empty<double>({3});
     res = xt::view(arr1, 1) + arr2;
-    std::cout<< arr1 << arr2 <<std::endl;
-    std::cout << res << std::endl;
+    std::cout << arr1 << "\n";
+    std::cout << arr2 << "\n";
+    std::cout << res << "\n";
+
+    if( xt::any(xt::abs(res - res_) > 1e-8) ) {
+        exit(2);
+    }
 
     return 0;
 }
