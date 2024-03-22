4037908c7e5b4b89a17df661dbdf5c6fcb24bafb
Gagandeep Singh <gdp.1807@gmail.com>
2024-03-22 15:35:35 +0530

    DEV: Use multiple torch::Tensor features in pytorch_01.cpp

--- integration_tests/pytorch_01.cpp
+++ integration_tests/pytorch_01.cpp
@@ -1,7 +1,19 @@
 #include <torch/torch.h>
 #include <iostream>
 
+void check(const torch::Tensor& tensor=torch::empty({1})) {
+    float array[5] = {4.0, 2.0, 2.0, 12.0, 2.0};
+    std::cout << tensor << std::endl;
+    if( torch::any(torch::abs(tensor - torch::from_blob(array, {5})) > 1e-6).item<bool>() ) {
+      exit(2);
+    }
+}
+
 int main() {
-  torch::Tensor tensor = torch::rand({2, 3});
-  std::cout << tensor << std::endl;
+    torch::Tensor tensor = torch::ones(5);
+    tensor[0] = 2.0;
+    tensor[3] = 6.0;
+    tensor = 2 * tensor;
+    check(tensor);
+    std::cout << tensor << std::endl;
 }
