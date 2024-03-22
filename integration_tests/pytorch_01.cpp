0c752db6eaca414e59029fdd771351a551a5e866
Gagandeep Singh <gdp.1807@gmail.com>
2024-03-23 03:26:22 +0530

    TEST: Improvided pytorch_01.cpp test

--- integration_tests/pytorch_01.cpp
+++ integration_tests/pytorch_01.cpp
@@ -3,8 +3,8 @@
 
 void check(const torch::Tensor& tensor=torch::empty({1})) {
     float array[5] = {4.0, 2.0, 2.0, 12.0, 2.0};
-    std::cout << tensor << std::endl;
-    if( torch::any(torch::abs(tensor - torch::from_blob(array, {5})) > 1e-6).item<bool>() ) {
+    std::cout << tensor << "\n";
+    if( torch::any(torch::abs(tensor - torch::from_blob(array, {5})) > 1e-8).item<bool>() ) {
       exit(2);
     }
 }
@@ -15,5 +15,5 @@ int main() {
     tensor[3] = 6.0;
     tensor = 2 * tensor;
     check(tensor);
-    std::cout << tensor << std::endl;
+    std::cout << tensor << "\n";
 }
