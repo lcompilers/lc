6c0f2de13573a4b88a61a4c00ad7eabb37f7fa69
Gagandeep Singh <gdp.1807@gmail.com>
2024-03-27 23:24:50 +0530

    TEST: Use newline character

--- integration_tests/array_14.cpp
+++ integration_tests/array_14.cpp
@@ -9,13 +9,13 @@ int main() {
 
 xt::xtensor<int, 2> array_const_1 = {{-14, 3, 0, -2}, {19, 1, 20, 21}};
 xt::xtensor_fixed<int, xt::xshape<2>> i23_shape = {4, 2};
-std::cout<< array_const_1 << std::endl;
+std::cout<< array_const_1 << "\n";
 array_const_1.reshape(i23_shape);
-std::cout << array_const_1 << std::endl;
-std::cout << array_const_1(0, 0) << " " << array_const_1(0, 1) << std::endl;
-std::cout << array_const_1(1, 0) << " " << array_const_1(1, 1) << std::endl;
-std::cout << array_const_1(2, 0) << " " << array_const_1(2, 1) << std::endl;
-std::cout << array_const_1(3, 0) << " " << array_const_1(3, 1) << std::endl;
+std::cout << array_const_1 << "\n";
+std::cout << array_const_1(0, 0) << " " << array_const_1(0, 1) << "\n";
+std::cout << array_const_1(1, 0) << " " << array_const_1(1, 1) << "\n";
+std::cout << array_const_1(2, 0) << " " << array_const_1(2, 1) << "\n";
+std::cout << array_const_1(3, 0) << " " << array_const_1(3, 1) << "\n";
 if( array_const_1(0, 0) != -14 ) {
     exit(2);
 }
